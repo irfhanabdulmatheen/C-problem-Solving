e1830b04802846ac4fd670e06e5af570157cf630
irfhanabdulmatheen <irfhan1214@gmail.com>
2024-08-11 18:43:04 +0530

    Create Sum of Integers.c

---
+++ C1/Sum of Integers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x, y, sum;
+    printf("\nInput first number: ");
+    scanf("%d", &x);
+    printf("\nInput second number: ");
+    scanf("%d", &y);
+    sum = x + y;
+    printf("\nSum = %d\n", sum);
+    return 0;
+}
+
