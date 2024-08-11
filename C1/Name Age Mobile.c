37bd83ffa2531560ec27825909e76cfa3557702f
irfhanabdulmatheen <irfhan1214@gmail.com>
2024-08-11 18:33:50 +0530

    Create Name Age Mobile.c

---
+++ C1/Name Age Mobile.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name   : Irfhan Abdul Matheen H\n");
+    printf("DOB    : March 08, 2006\n");
+    printf("Mobile : 99-9999988877\n");
+    return 0;
+}
