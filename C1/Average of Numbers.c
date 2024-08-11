12fa3d2674f51742a454b88acdea090bcc2c4235
irfhanabdulmatheen <irfhan1214@gmail.com>
2024-08-11 18:51:59 +0530

    Create Average of Numbers.c

---
+++ C1/Average of Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int x,y,z;
+    float sum;
+    printf("Number 1: ");
+    scanf("%d",&x);
+    printf("\nNumber 2: ");
+    scanf("%d",&y);
+    printf("\nNumber 3: ");
+    scanf("%d",&z);
+    sum=(x+y+z)/3;
+    printf("\nAverage %.3f ",yd);
+    printf("\n");
+    return 0;
+}
