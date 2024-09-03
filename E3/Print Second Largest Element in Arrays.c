3998ab1bf88e541ad5a1296b9dfaa9fdd4401aab
Irfhan <irfhan1214@gmail.com>
2024-09-03 14:53:22 +0530

    Create Print Second Largest Element in Arrays.c

---
+++ E3/Print Second Largest Element in Arrays.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int size,first,second,i;
+    printf("Enter the Size of The Array : ");
+    scanf("%d",&size);
+    int arr[size];
+    first=second=INT_MIN;
+    for(i=0;i<size;i++)
+    {
+        printf("Element %d : ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        if(arr[i] > first)
+        {
+            second = first;
+            first  = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != first)
+        {
+            second = arr[i];
+        }
+    }
+    if(second == INT_MIN)
+    {
+        printf("Not Exist");
+    }
+    else
+    {
+        printf("Second Largest Element : %d",second);
+    }
+}
