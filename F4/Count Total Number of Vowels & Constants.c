00531f63c1721c5524416731b6dc34c5f0e29792
Irfhan <irfhan1214@gmail.com>
2024-09-14 22:10:32 +0530

    Create Count Total Number of Vowels & Constants.c

---
+++ F4/Count Total Number of Vowels & Constants.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int i, len, vowel, cons;
+    printf("Input the string : ");
+    fgets(str, sizeof str, stdin);
+    vowel = 0;
+    cons = 0;
+    len = strlen(str);
+    for (i = 0; i < len; i++)
+        {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
+            vowel++;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            cons++;
+        }
+    }
+    printf("\nThe total number of vowel in the string is : %d\n", vowel);
+    printf("The total number of consonant in the string is : %d\n\n", cons);
+	return 0;
+}
